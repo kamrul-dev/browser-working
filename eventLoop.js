8d8ad5ebb22efca85bd418aa1205f5fb8af31657
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-03 11:20:25 +0600

    (advanced) JavaScript event loop and concurrency

---
+++ eventLoop.js
@@ -0,0 +1,9 @@
+/* 
+    (advanced) JavaScript event loop and concurrency
+
+    Event loop :  https://www.youtube.com/watch?v=8aGhZQkoFbQ 
+    Event loop article:  https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop 
+
+    topics: Event Loop, stack , queue
+
+*/
