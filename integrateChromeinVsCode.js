e9319aa16a7dbf6da58d9cd2e41c9d5044d6a6b5
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-03 11:34:00 +0600

    Integrate chrome devtool console with VS Code

---
+++ integrateChromeinVsCode.js
@@ -0,0 +1,9 @@
+/* 
+  Integrate chrome devtool console with VS Code:
+
+  step 1 : Clik on run and debug icon in left side of vs code
+  step 2 : click on Run and Debug
+  step 3 : open with chrome/ select chrome
+  step 4 : open debug console tab from the bottom of vs code.
+
+*/
